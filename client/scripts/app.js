67278322fe491f7907df48fd436b43c226a4b3de
sgenyk91 <sgenyk91@gmail.com>
2014-11-10 17:32:41 -0800

    Implemented up to chatroom behavior/add messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,67 +1,82 @@
-// YOUR CODE HERE:
-$(document).ready(function(){
-  $('#sendButton').on('click', sendMessage);
-  $('.requestMsg').on('click', retrieveMessages);
-  $('.refreshButton').on('click', function() {
-    $('.displayMessage').html('');
-    retrieveMessages();
-  });
-  setInterval(function() {
-    $('.displayMessage').html('');
-    retrieveMessages();
-  }, 30000);
-});
-
-var retrieveMessages = function() {
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-   // data: JSON.stringify(message),
-    contentType: 'application/json',
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('X-Parse-Application-Id', 'voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r');
-      xhr.setRequestHeader('X-Parse-REST-API-Key', 'QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf');
-    },
-    success: function (data) {
-      $('.displayMessage').html(JSON.stringify(data));
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    },
-    complete:function(){
-      console.log('complete');
-    }
-  });
+var message = {
+  username: 'Mel Brooks',
+  text: 'It\'s good to be the king',
+  roomname: 'lobby'
 };
+var app = {
+  init: function(){
+    $(document).ready(function(){
+      $('#sendButton').on('click', app.send);
+      $('.requestMsg').on('click', app.fetch);
 
-var sendMessage = function(){
- $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox.jsonp',
-    type: 'PUT',
-    data: {
-      "username":"Stephan",
-      "text":"hello stephan",
-      "roomname":"silvia"
-      //JSON.stringify(message)
-    },
-    dataType: 'jsonp',
-    contentType: 'application/json',
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('X-Parse-Application-Id', 'voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r');
-      xhr.setRequestHeader('X-Parse-REST-API-Key', 'QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf');
-    },
-    success: function (data) {
-      $('.displayMessage').html(JSON.stringify(data));
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    },
-    complete:function(){
-      console.log('complete');
-    }
-  });
+      $('.refreshButton').on('click', function() {
+        $('#chats').html('');
+        app.fetch();
+      });
+
+      setInterval(function() {
+        $('#chats').html('');
+        app.fetch();
+      }, 30000);
+    });
+  },
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  fetch : function() {
+    $.ajax({
+      // always use this url
+      url: app.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+  /*      var arr = data.results;
+        console.log('data', data);
+        for(var i = 11; i < arr.length; i++){
+         // console.log('test');
+          var chat = "";
+          chat += "username: " + arr[i].username + " text: " + arr[i].text
+              + " roomname: "+ arr[i].roomname;
+          $('#chats').append('<div>'+i+'====>'+chat+'</div>');
+        }*/
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      },
+      complete:function(){
+        console.log('complete');
+      }
+    });
+  },
+  send : function(message){
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+
+      success : function (data) {
+        $('#chats').html(JSON.stringify(data));
+        console.log('success!!!');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      },
+      complete:function(){
+        console.log('complete');
+      }
+    });
+  },
+  clearMessages : function() {
+    $('#chats').html('');
+  },
+  addMessage : function(message) {
+    var chat = "";
+    chat += "username: " + message.username + " text: " + message.text
+              + " roomname: "+ message.roomname;
+    $('#chats').append('<div>'+chat+'</div>');
+  }
 };
+app.init();
+
